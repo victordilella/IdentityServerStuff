807a4bf92803a85afbb0b95da7771a1e8d4ca57c
Victor DiLella <v.dilella@elsevier.com>
2017-01-27 11:56:57 -0500

    Client console app now successfully gets a token from the api

---
+++ src/ClientApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            doStuff();
+            Console.Read();
+        }
+
+        public static async void doStuff()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/identity");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
